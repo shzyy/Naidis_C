62495a6ab51698ca4401522f01c95ad55fe58e74
opilane <opilane@HP-CZC2349HTC>
2026-03-23 10:45:23 +0200

    .

---
+++ Naidis_C/Lemmikloomade register/Lemmikloom.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naidis_C
+{
+    class Lemmikloom
+    {
+        public string Nimi { get; set; }
+        public string Liik { get; set; }
+        public int Vanus { get; set; }
+
+        public Lemmikloom(string nimi, string liik, int vanus)
+        {
+            Nimi = nimi;
+            Liik = liik;
+            Vanus = vanus;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            List<Lemmikloom> loomad = new List<Lemmikloom>();
+
+            Console.WriteLine("Sisesta vähemalt 5 lemmiklooma:\n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Nimi: ");
+                string nimi = Console.ReadLine();
+
+                Console.Write("Liik (kass/koer jne): ");
+                string liik = Console.ReadLine();
+
+                Console.Write("Vanus: ");
+                int vanus = int.Parse(Console.ReadLine());
+
+                loomad.Add(new Lemmikloom(nimi, liik, vanus));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Kõik kassid:");
+            KuvaKassid(loomad);
+
+            double keskmine = KeskmineVanus(loomad);
+            Console.WriteLine($"\nKeskmine vanus: {keskmine:F2}");
+
+            var vanim = VanimLoom(loomad);
+            Console.WriteLine($"\nVanim loom: {vanim.Nimi} ({vanim.Vanus} a)");
+
+            Console.Write("\nSisesta nimi otsimiseks: ");
+            string otsing = Console.ReadLine();
+
+            var leitud = OtsiNimeJargi(loomad, otsing);
+
+            if (leitud != null)
+            {
+                Console.WriteLine($"Leitud: {leitud.Nimi}, {leitud.Liik}, {leitud.Vanus} a");
+            }
+            else
+            {
+                Console.WriteLine("Looma ei leitud.");
+            }
+        }
+
+        static void KuvaKassid(List<Lemmikloom> loomad)
+        {
+            foreach (var l in loomad)
+            {
+                if (l.Liik.Equals("kass", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"{l.Nimi} ({l.Vanus} a)");
+                }
+            }
+        }
+
+        static double KeskmineVanus(List<Lemmikloom> loomad)
+        {
+            return loomad.Average(l => l.Vanus);
+        }
+
+        static Lemmikloom VanimLoom(List<Lemmikloom> loomad)
+        {
+            return loomad.OrderByDescending(l => l.Vanus).First();
+        }
+
+        static Lemmikloom OtsiNimeJargi(List<Lemmikloom> loomad, string nimi)
+        {
+            return loomad.FirstOrDefault(l =>
+                l.Nimi.Equals(nimi, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ Naidis_C/hinnete analüüs/Opilane.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naidis_C.hinnete_analüüs
+{
+    internal class Opilane
+    {
+        public string Nimi { get; set; }
+        public List<int> Hinded { get; set; }
+
+        public opilane(string nimi, List<int> hinded)
+        {
+            Nimi = nimi;
+            Hinded = hinded;
+        }
+
+        public double Keskmine()
+        {
+            return Hinded.Average();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            List<Opilane> opilased = new List<Opilane>()
+        {
+            new Opilane("Mari", new List<int> { 5, 4, 5, 3 }),
+            new Opilane("Jaan", new List<int> { 3, 4, 2, 5 }),
+            new Opilane("Kati", new List<int> { 5, 5, 4, 5 })
+        };
+
+            Console.WriteLine("Õpilaste keskmised hinded:");
+
+            foreach (var opilane in opilased)
+            {
+                Console.WriteLine($"{opilane.Nimi}: {opilane.Keskmine():F2}");
+            }
+
+            var parim = opilased.OrderByDescending(o => o.Keskmine()).First();
+
+            Console.WriteLine($"\nParim õpilane: {parim.Nimi} ({parim.Keskmine():F2})");
+
+            Console.WriteLine("\nÕpilased sorteeritult (kõrgeimast madalaimani):");
+
+            var sorteeritud = opilased.OrderByDescending(o => o.Keskmine());
+
+            foreach (var opilane in sorteeritud)
+            {
+                Console.WriteLine($"{opilane.Nimi}: {opilane.Keskmine():F2}");
+            }
+        }
+    }
+}
+
